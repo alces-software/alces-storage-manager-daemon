bac6540a1f9a5b9aa7a870530085135431a0e254
James Muscat <james.muscat@alces-software.com>
2015-11-18 11:47:00 +0000

    Stub method for providing targets for a user.

--- lib/alces/storage-manager-daemon/control.rb
+++ lib/alces/storage-manager-daemon/control.rb
@@ -30,7 +30,17 @@ module Alces
       def authenticate?(options, user, pass)
         ::Rpam.auth(user, pass)
       end
-      
+
+      def targets_for(options, username)
+        {"STUB"  => {    
+            type: :remote,
+            dir_spec: :home,
+            address: "127.0.0.3:25268",
+            ssl: true
+          }
+        }
+      end
+
       PRIVATE_METHODS = [ :as,
                           :to_s,
                           :class,
