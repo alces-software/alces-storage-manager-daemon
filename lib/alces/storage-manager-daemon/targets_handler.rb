891fd87f55680bae53487d838dca63a1531786ed
James Muscat <james.muscat@alces-software.com>
2015-11-19 15:19:38 +0000

    Load targets configuration from system and user files.
    User targets are returned before system targets.

--- lib/alces/storage-manager-daemon/targets_handler.rb
+++ lib/alces/storage-manager-daemon/targets_handler.rb
@@ -1,24 +1,39 @@
+require 'yaml'
+
 module Alces
   module StorageManagerDaemon
     class TargetsHandler < BlankSlate
 
       def self.global_targets
-        @global_targets ||= {"STUB" => {    
-          type: :remote,
-          dir_spec: :home,
-          address: "127.0.0.3:25268",
-          ssl: true
-        }
-      }
+        globaldir = "/etc/xdg/clusterware/storage/"
+        @global_targets ||= TargetsHandler::load_from_directory(globaldir)
       end
 
       def user_targets(username)
-        {}
+        userdir = ::Dir.home + "/.config/clusterware/storage/"
+        TargetsHandler::load_from_directory(userdir)
       end
 
       def targets_for(username)
         user_targets(username).merge(TargetsHandler::global_targets)
       end
+      
+      private
+      
+      def self.load_from_directory(directory)
+        targets = {}
+        if ::Dir.exist?(directory)
+          ::DaemonKit.logger.debug("Looking for targets in " + directory)
+          ::Dir.glob(directory + "*.target.yml") { |targetFile| 
+            ::DaemonKit.logger.debug("Found " + targetFile)
+            target = ::YAML.load_file(targetFile)
+            targets[target.delete("name")] = target
+          }
+          ::DaemonKit.logger.debug("Found targets: " + targets.inspect)
+        end
+        targets
+      end
+      
     end
   end
 end
